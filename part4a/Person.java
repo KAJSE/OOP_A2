e5361ac96bd9255cfc3f68030ab6e239ca5c46b8
KAJSE <162324488+KAJSE@users.noreply.github.com>
2024-12-12 00:09:18 +0800

    Add files via upload

---
+++ part4a/Person.java
@@ -0,0 +1,61 @@
+public abstract class Person {
+    private String IDCardNumber;
+    private String name;
+    private int age;
+    private String gender;
+    private String phone;
+
+    public Person() {}
+
+    public Person(String IDCardNumber, String name, int age, String gender, String phone) {
+        this.IDCardNumber = IDCardNumber;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.phone = phone;
+    }
+
+    public String getIDCardNumber() {
+        return IDCardNumber;
+    }
+
+    public void setIDCardNumber(String IDCardNumber) {
+        this.IDCardNumber = IDCardNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void printDetails() {
+        System.out.println("IDCardNumber: " + IDCardNumber + ", Name: " + name + ", Age: " + age + ", Gender: " + gender + ", Phone: " + phone);
+    }
+}
